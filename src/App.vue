e0a7d47747f589745400dda70f29d6ac8eab6a48
GiacomoArena <giacomoarena28@gmail.com>
2023-06-26 18:03:52 +0200

    **Milestone 5**

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,52 @@
 <script>
-import {store} from './data/store';
+import { store } from './data/store';
+import axios from 'axios';
 
-  export default {
-    name: 'home',
+export default {
+  name: 'home',
 
-    methods:{
-
-      getApi(){
-        console.log(store.apiUrl);
-      }
-    },
-    mounted(){
-      this.getApi();
+  data() {
+    return {
+      portfolios: []
     }
+  },
+  methods: {
+    getApi() {
+      console.log(store.apiUrl);
+      axios.get(store.apiUrl).then(results => {
+        this.portfolios = results.data
+        console.log(this.portfolios);
+      })
+    }
+  },
+
+  mounted() {
+    this.getApi();
   }
+}
 </script>
 
 <template>
-  <div>
-    <h3>Home</h3>
+  <div class="container">
+    <h2>portfolios</h2>
+    
+    <table class="table table-striped ">
+      <thead>
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Name</th>
+          <th scope="col">Surname</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in portfolios" :key="item.id">
+
+          <td>{{ item.title }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.surname }}</td>
+        </tr>
+        </tbody>
+    </table>
   </div>
 </template>
 
