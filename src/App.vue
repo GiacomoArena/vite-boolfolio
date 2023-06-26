c8fe487711a85498071355fe130b302b13f52113
GiacomoArena <giacomoarena28@gmail.com>
2023-06-26 17:21:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h3>Home</h3>
+  </div>
+</template>
+
+<style lang="scss" >
+@import "./scss/Main.scss";
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+
+import {store} from './data/store';
+import App from './App.vue';
+
+createApp(App).mount('#app')
