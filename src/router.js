dce1f0ac00bfd4a7e1535c92b294f90cb6c9fb2e
GiacomoArena <giacomoarena28@gmail.com>
2023-06-27 15:58:35 +0200

    create some route and start to amke router link

--- src/router.js
+++ src/router.js
@@ -1,7 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
+import Home from './pages/Home.vue';
+import Portfolios from './pages/Portfolios.vue';
+
 const router = createRouter({
 
   history:createWebHistory(),
-  
+
+  routes:[
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/portfolios',
+      name: 'portfolios',
+      component: Portfolios
+    },
+    
+  ]
 
 })
+
+export {router}
