3827980df17c8348062a152ef0f6345c22a86099
GiacomoArena <giacomoarena28@gmail.com>
2023-06-27 15:25:46 +0200

    create router.js and pages

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,24 @@ export default {
 
   data() {
     return {
-      portfolios: []
+      portfolios: [],
+      links: []
     }
   },
   methods: {
-    getApi() {
-      console.log(store.apiUrl);
-      axios.get(store.apiUrl).then(results => {
-        this.portfolios = results.data
-        console.log(this.portfolios);
+    getApi(endpoint) {
+      axios.get(endpoint).then(results => {
+
+        this.portfolios = results.data.data
+        this.links = results.data.links
+
+       // console.log(this.portfolios);
       })
     }
   },
 
   mounted() {
-    this.getApi();
+    this.getApi(store.apiUrl);
   }
 }
 </script>
@@ -33,6 +36,7 @@ export default {
     <table class="table table-striped ">
       <thead>
         <tr>
+          <th scope="col">Id</th>
           <th scope="col">Title</th>
           <th scope="col">Name</th>
           <th scope="col">Surname</th>
@@ -41,12 +45,21 @@ export default {
       <tbody>
         <tr v-for="item in portfolios" :key="item.id">
 
+          <td>{{ item.id }}</td>
           <td>{{ item.title }}</td>
           <td>{{ item.name }}</td>
           <td>{{ item.surname }}</td>
         </tr>
         </tbody>
     </table>
+    <div>
+      <button class="btn btn-dark"
+      v-for="(link, i) in links" :key="i"
+      v-html="link.label"
+      @click="getApi(link.url)">
+      
+      </button>
+    </div>
   </div>
 </template>
 
