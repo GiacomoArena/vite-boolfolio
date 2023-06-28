c2bf447def3a4f4265184dfb1d8396d55d172b8e
GiacomoArena <giacomoarena28@gmail.com>
2023-06-28 15:24:57 +0200

    make filter by type and reset button

--- src/pages/Portfolios.vue
+++ src/pages/Portfolios.vue
@@ -14,7 +14,7 @@ export default {
     }
   },
   methods: {
-    getApi(endpoint) { 
+    getApi(endpoint=store.apiUrl) {
       axios.get(endpoint).then(results => {
 
         this.portfolios = results.data.data
@@ -24,51 +24,86 @@ export default {
         //console.log(results.data);
       })
     },
-    getType() { 
+    getType() {
       axios.get(store.typeUrl).then(results => {
         this.type = results.data
-        console.log( this.type);
+        console.log(this.type);
       })
-    }
-  },
-  gettypeid(id){
-    this.getApi(store.apiUrl +'/by-type/'+id)
+    },
+    gettypeid(id) {
+      this.getApi(store.apiUrl + '/by-type/' + id)
+    },
   },
 
   mounted() {
-    this.getApi(store.apiUrl);
+    this.getApi();
     this.getType();
   }
 }
 </script>
 
 <template>
-  <div class="container card">
-    <span>
-      <h2>portfolios</h2> 
-      <!-- <button v-for="item in type" :key="item.id" 
-      @click="gettypeid(item.id)">
-      {{ item.type }}
-    </button> -->
-    </span> 
+  <div class="container card d-flex">
+    <h2>portfolios</h2>
+    <div class="main-container d-flex">
 
-    <ol class="list-group" v-for="item in portfolios" :key="item.id">
-      <li class="list-group-item d-flex justify-content-between align-items-start">
-        <div class="ms-2 me-auto">
-          <div class="fw-bold">{{ item.id }}-{{ item.title }}</div>
-          <span>{{ item.name }} {{ item.surname }}</span>
-          <span class="badge bg-dark ms-3">{{ item.type.type }}</span>
-          <span class="badge bg-secondary ms-3" v-for="(tech, i) in item.technologies" :key="i">{{ tech.name }}</span>
+      <div class="left-container ">
+        <ol class="list-group" v-for="item in portfolios" :key="item.id">
+          <li class="list-group-item d-flex justify-content-between align-items-start">
+            <div class="ms-2 me-auto">
+              <div class="fw-bold">{{ item.id }}-{{ item.title }}</div>
+              <span>{{ item.name }} {{ item.surname }}</span>
+              <span class="badge bg-dark ms-3">{{ item.type.type }}</span>
+              <span class="badge bg-secondary ms-3" v-for="(tech, i) in item.technologies" :key="i">{{ tech.name }}</span>
+            </div>
+          </li>
+        </ol>
+      </div>
+      
+      <div class="right-container ">
+        <div>
+          <button class="btn btn-danger m-2"
+          @click="getApi()">
+            <i class="fa-solid fa-rotate"></i>
+          </button>
+          <i class="fa-solid fa-filter"></i>
         </div>
-      </li>
-    </ol>
-
-    <div class="m-2">
-      <button class="btn btn-dark m-1" v-for="(link, i) in links" :key="i" v-html="link.label" @click="getApi(link.url)">
-
-      </button>
+        <button class="btn my-btn" v-for="item in type" :key="item.id" @click="gettypeid(item.id)">
+          {{ item.type }}
+        </button>
+      </div>
+      
     </div>
+
+      <div class="m-2">
+        <button class="btn btn-dark m-1" v-for="(link, i) in links" :key="i" v-html="link.label"
+          @click="getApi(link.url)">
+        </button>
+      </div>
+
+
   </div>
 </template>
 
-<style lang="scss" ></style>
+<style lang="scss" scoped >
+
+  .main-container{
+    .left-container{
+      width: 100%;
+    }
+    .right-container{
+      overflow-y: auto;
+      height: 500px;
+      width: 250px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .my-btn{
+        background-color: rgba(56, 35, 35, 0.788);
+        color: white;
+        width: 150px;
+        margin: 5px;
+      }
+    }
+  }
+</style>
