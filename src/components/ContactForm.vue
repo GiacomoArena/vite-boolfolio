8b7585bc6d22936257891aa1b0e699f6acf2b17a
GiacomoArena <giacomoarena28@gmail.com>
2023-06-29 17:15:32 +0200

    update commit

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,51 +1,69 @@
-  <script>
-  export default {
-    name:'ContactForm',
+<script>
+import axios from 'axios';
+import { store } from '../data/store';
 
-    data(){
-      return{
-        name:'',
-        email:'',
-        message:'',
-      }
-    },
-    methods:{
+export default {
+  name: 'ContactForm',
 
-      sendForm(){
-        const data = {
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      errors: {},
+    }
+  },
+  methods: {
+
+    sendForm() {
+      const data = {
         name: this.name,
         email: this.email,
         message: this.message,
-        }
-        console.log(data);
       }
+      axios.post(store.contactUrl, data)
+        .then(result => {
+          console.log(result.data.errors);
+          if (result.data && result.data.errors) {
+            this.errors = result.data.errors;
+          } else {
+            this.errors = {};
+          }
+        })
     }
   }
-  </script>
+}
+</script>
 
 
 <template>
-  
   <form @submit.prevent="sendForm()">
-  <div class="mb-3">
     <div class="mb-3">
-      <label  class="form-label">Name</label>
-      <input v-model.trim="name" type="text" class="form-control" name="name" placeholder="Name">
+      <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input v-model.trim="name" type="text" :class="{ 'error': errors.name }" class="form-control" name="name"
+          placeholder="Name">
+          <span class="badge bg-danger m-2" v-html="errors.name"></span>
+      </div>
+      <label class="form-label">Email address</label>
+      <input v-model.trim="email" type="email" :class="{ 'error': errors.email }" class="form-control"
+        id="exampleInputEmail1" placeholder="Email">
+        <span class="badge bg-danger m-2" v-html="errors.email"></span>
     </div>
-    <label  class="form-label">Email address</label>
-    <input v-model.trim="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-  </div>
-  <div class="mb-3">
-      <label  class="form-label">Message</label>
-      <textarea v-model.trim="message" type="text" class="form-control" name="name" placeholder="Write something"></textarea>
+    <div class="mb-3">
+      <label class="form-label">Message</label>
+      <textarea v-model.trim="message" type="text" :class="{ 'error': errors.message }" class="form-control" name="name"
+        placeholder="Write something"></textarea>
+        <span class="badge bg-danger m-2" v-html="errors.message"></span>
     </div>
-  
-  <button type="submit" class="btn btn-dark">Send</button>
-</form>
 
+    <button type="submit" class="btn btn-dark m-3">Send</button>
+  </form>
 </template>
 
 
 <style lang="scss" scoped >
-
+.error {
+  border: 1px solid brown
+}
 </style>
--- src/data/store.js
+++ src/data/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
 
 
   apiUrl: 'http://127.0.0.1:8000/api/portfolios',
+  contactUrl:'http://127.0.0.1:8000/api/contacts',
   typeUrl:'http://127.0.0.1:8000/api/portfolios/type',
   techUrl:'http://127.0.0.1:8000/api/portfolios/tech'
 })
